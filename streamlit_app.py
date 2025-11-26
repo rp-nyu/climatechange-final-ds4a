b4a189d4131a555f6d382ffd0811f1138e91e076
John <156053292+johnlamair@users.noreply.github.com>
2025-11-26 12:44:06 +0000

    added introduction page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.sidebar.title("Pages:")
+selected_page = st.sidebar.radio("Select Page", ["Introduction", "Visualization", "Prediction", "Conclusion"])
+
+st.title("Climate Change Prediction")
+
+if selected_page == "Introduction": 
+    st.link_button("Github Repo", "https://github.com/rp-nyu/climatechange-final-ds4a")
+    st.image("drid-polar-bear.jpg", use_container_width=True)
+
+    st.markdown("### Introduction")
